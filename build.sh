219a8cb68005e6034965edf8939e309bee05df18
Robert Long <robert@robertlong.me>
2018-04-06 14:26:38 -0700

    Reduce wasm output size

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 # Build the .wasm module
-cargo +nightly build --target wasm32-unknown-unknown
+cargo +nightly build --release --target wasm32-unknown-unknown
 
 # Generate the rust/js bindings
-wasm-bindgen ./target/wasm32-unknown-unknown/debug/wasm_webgl_ecs.wasm --out-dir .
+wasm-bindgen ./target/wasm32-unknown-unknown/release/wasm_webgl_ecs.wasm --out-dir .
+
+# Reduce size of .wasm output
+wasm-gc ./wasm_webgl_ecs_bg.wasm ./wasm_webgl_ecs_bg.wasm
+
+# TODO use wasm-opt to optimize output
 
 # Format the generated js
 prettier --write wasm_webgl_ecs.js
